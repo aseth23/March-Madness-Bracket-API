49f00ebd2be383256252e7a37b4ef5c3ea438f92
Armaan Seth <armaanseth@Armaans-MacBook-Pro.local>
2026-02-15 22:21:42 -0500

    Persist sqlite db on Render disk

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./brackets.db"
+DATABASE_URL = "sqlite:////var/data/brackets.db"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
